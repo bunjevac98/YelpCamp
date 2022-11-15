c55e4e884f4f47d74242ad539e4c2469bb97b205
bunjevac98 <basic.aco@hotmail.com>
2022-11-15 14:46:46 +0100

    CRUD Finished
    CRUD operation ende

--- app.js
+++ app.js
@@ -77,6 +77,17 @@ app.put('/campgrounds/:id', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+
+
+})
+
+
+
+
 app.listen(3000, () => {
     console.log('Server na portu 3000');
 })
