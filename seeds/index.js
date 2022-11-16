1d39448da6908ebe92a45f224f5ca285004de547
bunjevac98 <basic.aco@hotmail.com>
2022-11-16 22:28:28 +0100

    style za CRUD operacije
    -dodat boots za stranice koje smo napravili do sada

--- seeds/index.js
+++ seeds/index.js
@@ -26,14 +26,19 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground(
             {
                 location: `${cities[random1000].city}, ${cities[random1000].state}`,
-                title: `${sample(descriptors)}, ${sample(places)}`
+                title: `${sample(descriptors)}, ${sample(places)}`,
+                image: 'https://source.unsplash.com/collection/483251/480x480',
+                description: 'Bilo kakv text samo da je dovoljno dugacak da mozemo da ga stavimo u dva reda tako da ovde apsolutno nema veze sta pise, samo da ne pisu neke gluposti i da ga mozemo pravilno dodavat',
+                price
             })
         await camp.save();
     }
 }
+
 //OVO NAM Pomaze samo da dodamo neke proizvode u nasu bazu i da mozemo manipulisati sa njima
 seedDB().then(() => {
     mongoose.connection.close();
