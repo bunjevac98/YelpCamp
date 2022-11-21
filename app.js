9e9d2a9311ccf41b4422e507653d5ee3dd1bf34b
bunjevac98 <basic.aco@hotmail.com>
2022-11-21 19:48:36 +0100

    Review dodat
    Dodato ocenjivanje i komentarisanje revjua

--- app.js
+++ app.js
@@ -6,13 +6,17 @@ const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError')
 const catchAsync = require('./utils/catchAsync');
 const Campground = require('./models/campground');
+const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const { nextTick } = require('process');
 const Joi = require('joi');
 const { runInNewContext } = require('vm');
+const { validate } = require('./models/campground');
+const Review = require('./models/review')
+
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
-    // useCreateIndex: true,
+    // useCreateIndex: true, 
     useUnifiedtopology: true
 })
 
@@ -34,16 +38,11 @@ app.use(express.urlencoded({ extended: true }))
 //ovo je za uprate U ONOM FORMU KOD EDITA AKO SE OVDE ZOVE _METHOD MORA I TAMO TO JE POVESANO
 app.use(methodOverride('_method'));
 
+
+
+
 const validateCmapground = (req, res, next) => {
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required()
-        }).required()
-    })
+
     const { error } = campgroundSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',');
@@ -54,7 +53,16 @@ const validateCmapground = (req, res, next) => {
     }
 }
 
-
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    else {
+        next();
+    }
+}
 
 
 
@@ -79,6 +87,8 @@ app.get('/campgrounds/new', (req, res) => {
 })
 
 app.post('/campgrounds', validateCmapground, catchAsync(async (req, res, next) => {
+    // //#region region 
+
     //BODY JE PRAZAN I MI MORAMO DA GA PARSIRAMO TAKO DA TREBA DA 
     //DEKODIRAMO ONO STO SMO NAPISALI A TO SE NALAZI OVDEEEEEEEEEEEE
     //u ovom body se nalazi nas objekat
@@ -86,7 +96,7 @@ app.post('/campgrounds', validateCmapground, catchAsync(async (req, res, next) =
     if (!req.body.campground) {
         throw new ExpressError('Invalid Campground Data', 400);
     }*/
-
+    // //#endregionendregion
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -94,17 +104,17 @@ app.post('/campgrounds', validateCmapground, catchAsync(async (req, res, next) =
 
 ///KOD OVOGA PAZITI JER KAD DODJE DO OVOGA TRAZICE TU TUTU TAKO DA JE REDOSLED GET-A VEOMA BITAN
 app.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
+    console.log(campground);
     res.render('campgrounds/show', { campground });
 }))
 
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res, next) => {
-
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
 }))
 
-app.put('/campgrounds/:id', catchAsync(async (req, res, next) => {
+app.put('/campgrounds/:id', validateCmapground, catchAsync(async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
@@ -115,6 +125,30 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }))
+
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+}));
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    //sada brisemo iz campgrounda taj jedan review
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { review: reviewId } });
+    await Review.findByIdAndDelete(reviewId)
+    res.redirect(`/campgrounds/${id}`);
+
+}))
+
+
+
+
+
 
 //kada trazimo url koji ne postoji REDOSLED BITAAN OVO ULAZI SAMO KADA NISTA NIJE NASAO
 app.all('*', (req, res, next) => {
