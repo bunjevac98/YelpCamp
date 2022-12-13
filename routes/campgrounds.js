73e03b25a21b79e4ab4c27a270e38e08b689e908
bunjevac98 <basic.aco@hotmail.com>
2022-12-13 14:26:43 +0100

    Router.route()
    lepsi izgled rutiranja

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -6,27 +6,27 @@ const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCmapground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds')
 
-
-
 //U RENDER IDE PUTANJA OD EJS KOJI RENDERUJEMO
 //kao sto je u C# Views
 //PAZITI KOD RENDERA NE IDE JEBEN "/" POSLE SVEGA
-router.get('/', catchAsync(campgrounds.index))
+
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCmapground, catchAsync(campgrounds.createCampground));
+
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
+
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCmapground, catchAsync(campgrounds.upradeCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+
+
 
 
 ///ovaj get kada se unesu podaci vraca post metodu a ona se nalazi ispod ovoga
-router.get('/new', isLoggedIn, campgrounds.renderNewForm)
-
-router.post('/', isLoggedIn, validateCmapground, catchAsync(campgrounds.createCampground))
-
-///KOD OVOGA PAZITI JER KAD DODJE DO OVOGA TRAZICE TU TUTU TAKO DA JE REDOSLED GET-A VEOMA BITAN
-router.get('/:id', catchAsync(campgrounds.showCampground))
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
-
-router.put('/:id', isLoggedIn, isAuthor, validateCmapground, catchAsync(campgrounds.upradeCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 
 
--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,14 @@ const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const users = require('../controllers/users');
 
-
-router.get('/register', users.renderRegister)
-
-
-router.post('/register', catchAsync(users.register))
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register))
 
 
-router.get('/login', (req, res) => {
-    res.render('users/login');
-})
-
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', failureMessage: 'Morate ponovo niste se ulogovali' }), users.login)
+router.route('/login')
+    .get((req, res) => { res.render('users/login'); })
+    .post(passport.authenticate('local', { failureFlash: true, failureRedirect: '/login', failureMessage: 'Morate ponovo niste se ulogovali' }), users.login)
 
 //ima greska mozda
 //logout je sada funkcija koja je asinhrona proveriti ovo sta se desava jer je jako komplikovano
