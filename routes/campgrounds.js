76b5e7458b09570719f1ebdda2d8f69aea93b79e
bunjevac98 <basic.aco@hotmail.com>
2022-12-13 13:26:07 +0100

    Refactorin campgroundS controller
    premestili smo campground u svoj kontroler gde ce se nalaziti sva potrebna logika

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,74 +3,30 @@ const router = express.Router();
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const catchAsync = require('../utils/catchAsync');
-const { campgroundSchema } = require('../schemas.js');
 const { isLoggedIn, validateCmapground, isAuthor } = require('../middleware');
-
+const campgrounds = require('../controllers/campgrounds')
 
 
 
 //U RENDER IDE PUTANJA OD EJS KOJI RENDERUJEMO
 //kao sto je u C# Views
 //PAZITI KOD RENDERA NE IDE JEBEN "/" POSLE SVEGA
-router.get('/', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds });
-})
+router.get('/', catchAsync(campgrounds.index))
 
 
 ///ovaj get kada se unesu podaci vraca post metodu a ona se nalazi ispod ovoga
-router.get('/new', isLoggedIn, (req, res) => {
-    res.render('campgrounds/new');
-})
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
-router.post('/', isLoggedIn, validateCmapground, catchAsync(async (req, res, next) => {
-    // //#region region 
-    const campground = new Campground(req.body.campground);
-    campground.author = req.user._id;
-    await campground.save();
-    req.flash('success', 'Successfuly made a new campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}))
+router.post('/', isLoggedIn, validateCmapground, catchAsync(campgrounds.createCampground))
 
 ///KOD OVOGA PAZITI JER KAD DODJE DO OVOGA TRAZICE TU TUTU TAKO DA JE REDOSLED GET-A VEOMA BITAN
-router.get('/:id', catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id).populate({
-        path: 'reviews',
-        populate: {
-            path: 'author'
-        }
-    }).populate('author');
-    console.log(campground);
-    if (!campground) {
-        req.flash('error', 'Cannoct find campground');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/show', { campground });
-}))
+router.get('/:id', catchAsync(campgrounds.showCampground))
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground) {
-        req.flash('error', 'Cannoct find campground');
-        return res.redirect('/campgrounds');
-    }
-    res.render('campgrounds/edit', { campground });
-}))
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
-router.put('/:id', isLoggedIn, isAuthor, validateCmapground, catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    req.flash('success', 'Successfuly updated campground!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
+router.put('/:id', isLoggedIn, isAuthor, validateCmapground, catchAsync(campgrounds.upradeCampground));
 
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res, next) => {
-
-    const campground = await Campground.findByIdAndDelete(id);
-    req.flash('success', 'Successfuly deleted campground!');
-    res.redirect('/campgrounds');
-}))
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
 
 
